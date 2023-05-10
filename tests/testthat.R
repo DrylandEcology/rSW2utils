146e3adafc066ad10aa6be306c4809c3c036f774
Daniel Schlaepfer <daniel.schlaepfer@alumni.ethz.ch>
2023-05-10 17:48:55 +0200

    Lint [unused_import_linter]
    -> Package 'rSW2utils' is attached but never used.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,4 @@
 library("testthat") # nolint: undesirable_function_linter
-library("rSW2utils") # nolint: undesirable_function_linter
+library("rSW2utils") # nolint: unused_import_linter
 
 test_check("rSW2utils")
